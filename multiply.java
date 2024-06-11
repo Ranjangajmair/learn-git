197a1e59e063a1badd0871609200f6c78d59ae61
Ranjan Gajmair <131667074+Ranjangajmair@users.noreply.github.com>
2024-06-11 12:16:59 +0545

    added multiplication numbers

--- multiply.java
+++ multiply.java
@@ -1,7 +1,7 @@
 public class sum {
     public static void main(string[] args) {
-        int a = 2;
-        int b= 3;
+        int a = 12;
+        int b= 16;
         system.out.print(a*b);
     }
 }
